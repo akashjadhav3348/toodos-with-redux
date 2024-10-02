f69e5c9fffaa2771600cd183a96a55809b4f634a
Akash Jadhav <akashjadhav3348@gmail.com>
2024-10-02 22:52:10 +0530

    new

--- src/componants/ToodosItem.jsx
+++ src/componants/ToodosItem.jsx
@@ -8,7 +8,7 @@ const ToodosItem = ( { todoTest , index}) => {
         
     return (
         <>
-        <div className='d-flex justify-content-between my-3 align-items-center bg-info p-3 rounded gap-3 text-white shadow'>
+        <div className='d-flex justify-content-between my-3 align-items-center bg-info p-3 rounded gap-3 shadow'>
                 <div className='fs-5 fw-bold'> {todoTest} </div>
             <div className='d-flex gap-3'>
                 <button className='btn btn-success' onClick={ () => dispalyToodos(edit({text: todoTest, index: index}))}>Edit</button>
