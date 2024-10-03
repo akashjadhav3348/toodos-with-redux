184e391ca31cd5f40d8398c92cb9ff58fda2ba4d
Akash Jadhav <akashjadhav3348@gmail.com>
2024-10-03 11:59:25 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import ToodoList from './componants/ToodoList';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import Nav from './componants/Nav';
 
 function App() {
 
   return (
+    
     <Provider store={store}>
+      <Nav />
       <ToodoList />
     </Provider>
   )
