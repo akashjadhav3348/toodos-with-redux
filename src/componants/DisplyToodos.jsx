77d446001e6198cc14692a951e72cb435741b51b
Akash Jadhav <akashjadhav3348@gmail.com>
2024-10-03 10:49:46 +0530

    new Changes

--- src/componants/DisplyToodos.jsx
+++ src/componants/DisplyToodos.jsx
@@ -7,7 +7,7 @@ const DisplyToodos = () => {
     return (
         <>
             <div>
-                {(todos.length === 0) ? (<h6 className='text-center mt-3 text-danger fw-bold fs-1 border-bottom border-primary pb-3' >No Display Toodo</h6>) : null}
+                {(todos.length === 0) ? (<h4 className='text-center mt-3 text-danger fw-bold fs-4 border-bottom border-primary pb-3' >No Display Toodo</h4>) : null}
                 {
                     todos.map((item, index) => <ToodosItem todoTest={item} key={index} index={index} />)
                 }
